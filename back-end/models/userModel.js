ec6558229e4c0582b581b24c047fa64a1a0c57a7
Gustavo <ggteodor07@gmai.com>
2020-09-23 10:48:13 -0300

    fixing erros lint 2

--- back-end/models/userModel.js
+++ back-end/models/userModel.js
@@ -27,7 +27,8 @@ const singupUser = async (name, email, password, role) => connect()
     .getTable('users')
     .insert(['email', 'password', 'name', 'role'])
     .values(email, password, name, role)
-    .execute());
+    .execute())
+  .then(() => { name, email, password, role });
 
 module.exports = {
   getAllUsers,
