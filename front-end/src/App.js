4009fa153c0441799d4524549d89a9f5def3be72
brenomesquita <brenomesquitaribeiro@gmail.com>
2020-09-23 18:12:11 -0300

    connection

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,13 +4,12 @@ import './App.css';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import UserProfile from './pages/UserProfile';
-import Home from './pages/Home';
 
 function App() {
   return (
     <Router>
       <Switch>
-        <Route exact path="/" component={ Home } />
+        <Route exact path="/" component={ login } />
         <Route exact path="/login" component={ Login } />
         <Route exact path="/register" component={ Register } />
         <Route exact path="/profile" component={ UserProfile } />
