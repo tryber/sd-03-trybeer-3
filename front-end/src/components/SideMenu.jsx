e43baf7d5681c13bba967cfa5bb1ab28a8cf2ce5
Clayton Pereira <cpereiramt@gmail.com>
2020-09-24 19:35:54 -0400

    Salvando alteraçoes projeto

--- front-end/src/components/SideMenu.jsx
+++ front-end/src/components/SideMenu.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 function SideMenu() {
     return (
         <div>
-            <h1>SideMenu </h1>
+            <h1 data-testid="side-menu-item-products"> Produtos </h1>
+            <h1 data-testid="side-menu-item-my-orders"> Meus Pedidos </h1>
+            <h1 data-testid="side-menu-item-my-profile"> Meu Perfil </h1>
         </div>
     )
 }
--- front-end/src/components/TopMenu.jsx
+++ front-end/src/components/TopMenu.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 function TopMenu(props) {
     return (
-        <div data-testid="top-title">
+        <div >
             <img data-testid="top-hamburguer" /> 
-            <h1>TopMenu</h1>
+           <h1 data-testid="top-title">{props.title}</h1>           
         </div>
     )
 }
