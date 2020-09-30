deb3ae2217a92f083f6d0f10d66d67058210fe10
Clayton Guimaraes Leite Pereira <claytonpereira@mtprev.local>
2020-09-30 12:05:10 -0400

    test coverage 23.56% das linhas Frontend

--- front-end/src/pages/AdminOrders.test.js
+++ front-end/src/pages/AdminOrders.test.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import { fireEvent, getByTestId, render} from '@testing-library/react';
 import AdminOrders from './AdminOrders';
 import renderWithRouter from '../renderWithRouter';
+import AdminOrdersDetails from './AdminOrdersDetails';
 
 describe('Pagina de controle de pedidos, usuário administrador', () => {
   test('Testando o componente renderiza corretamente', () => {
-    const { getByTestId , container} = renderWithRouter(<AdminOrders />);
-    const state = getByTestId(container, 'purchase');
-    console.log(state);    
+    const AdminOrdersPage = renderWithRouter(<AdminOrders />);
+    expect(AdminOrdersPage);
   });
 });
--- front-end/src/pages/Login.test.js
+++ front-end/src/pages/Login.test.js
@@ -11,3 +11,14 @@ test('Testa o onClick no botão de registro na tela login', () => {
     expect(pathname).toBe('/register');
   });
   
+  test('Testa o onClick no botão de registro na tela login', async () => {
+    const { getByTestId, history } = renderWithRouter(<Login />);
+    const inputEmail = getByTestId('email-input');
+    fireEvent.change(inputEmail, { target: { value: 'a' } })
+    const inputPassword = getByTestId('password-input');
+    fireEvent.change(inputPassword, { target: { value: '00000' } })
+    const buttonLogin = getByTestId('signin-btn');
+    await fireEvent.click(buttonLogin);
+    const pathname = history.location.pathname;
+    expect(pathname).toBe('/');
+  });
