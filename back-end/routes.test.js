cb54c0bbbd6239691f125d3543c31c813c026699
Clayton Pereira <cpereiramt@gmail.com>
2020-10-03 18:07:09 -0400

    Test coverage Backend 65.11% das linhas

--- back-end/routes.test.js
+++ back-end/routes.test.js
@@ -6,7 +6,7 @@ const connect = require('./models/connection')
 
 }) 
 describe('Teste de Post', () => {
-  it('Testando login', async () => {
+  it('Testando o login correto', async () => {
     const res = await request(app)
       .post('/login')
       .send({
@@ -14,5 +14,22 @@ describe('Teste de Post', () => {
         password: '12345678',
       })
     expect(res.statusCode).toEqual(200)
+  })
+  it('Testando o login incorreto', async () => {
+    const res = await request(app)
+      .post('/login')
+      .send({
+        email: 'bruno.batista@gmail.com',
+        password: '1234',
+      })
+    expect(res.statusCode).toEqual(401)
+  })
+  it('Testando o login sem email', async () => {
+    const res = await request(app)
+      .post('/login')
+      .send({
+        password: '1234',
+      })
+    expect(res.statusCode).toEqual(500)
   })
 })
