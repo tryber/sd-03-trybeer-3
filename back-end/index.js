1b9483eaaa04400778e96785f62bde59b25b58b2
Clayton Pereira <cpereiramt@gmail.com>
2020-10-03 17:46:12 -0400

    Test coverage Backend 62.59% das linhas

--- back-end/index.js
+++ back-end/index.js
@@ -6,6 +6,7 @@ const bodyParser = require('body-parser');
 const routes = require('./routes');
 
 const app = express();
+
 app.use(bodyParser.json());
 app.use(cors());
 app.use('/login', routes.loginRoute);
@@ -18,10 +19,11 @@ app.use('/images', express.static('images'));
 
 app.use((error, _req, res, _next) => {
   const { message, status } = error;
+  
   if (status < 500) {
     return res.status(status).json({ message });
   }
-  res.status(500).send('Something broke!');
+  res.status(500).json({ message });
 });
 
 const PORT = process.env.PORT || 3001;
--- back-end/routes.test.js
+++ back-end/routes.test.js
@@ -1,21 +1,18 @@
 const request = require('supertest')
 const  app  = require('./index')
 const connect = require('./models/connection')
-afterEach(async () =>{
+ afterEach(async () =>{
   await connect().catch(error => console.log(error)) 
 
-})
-
+}) 
 describe('Teste de Post', () => {
   it('Testando login', async () => {
     const res = await request(app)
       .post('/login')
       .send({
-        email: 'teste@teste.com.br',
-        password: '123456',
+        email: 'bruno.batista@gmail.com',
+        password: '12345678',
       })
-    expect(res.statusCode).toEqual(201)
-    expect(res.body).toHaveProperty('post')
-    done()
+    expect(res.statusCode).toEqual(200)
   })
 })
