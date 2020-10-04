923acc4c7047045f5192498073cf48a3c898c8ac
Clayton Pereira <cpereiramt@gmail.com>
2020-10-04 17:02:34 -0400

    Solving lint problems

--- front-end/src/pages/AdminOrders.test.js
+++ front-end/src/pages/AdminOrders.test.js
@@ -6,15 +6,8 @@ import renderWithRouter from '../renderWithRouter';
 import AdminOrdersDetails from './AdminOrdersDetails';
 
 describe('Pagina de controle de pedidos, usuário administrador', () => {
-<<<<<<< HEAD
-  test('Testando onClick', () => {
-    const  { getAllByText } = renderWithRouter(<AdminOrders  />);
-    const ordersDivH2 = getAllByText(/Pedidos/i);
-    expect(ordersDivH2[0]).toBeInTheDocument();
-=======
   test('Testando o componente renderiza corretamente', () => {
     const AdminOrdersPage = renderWithRouter(<AdminOrders />);
     expect(AdminOrdersPage);
->>>>>>> deb3ae2217a92f083f6d0f10d66d67058210fe10
   });
 });
