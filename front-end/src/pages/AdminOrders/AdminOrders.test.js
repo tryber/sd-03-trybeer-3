76dc013838762456b1d191bb2cbb8947501f0833
Clayton Pereira <cpereiramt@gmail.com>
2020-10-23 21:08:30 -0400

    configurando enzyme

--- front-end/src/pages/AdminOrders/AdminOrders.test.js
+++ front-end/src/pages/AdminOrders/AdminOrders.test.js
@@ -2,11 +2,20 @@ import React from 'react';
 import {fireEvent, render} from '@testing-library/react';
 import AdminOrders from './Index';
 import renderWithRouter from '../../renderWithRouter';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+ 
+Enzyme.configure({ adapter: new Adapter() });
 
 describe('Pagina de checkout de um pedido', () => {
-  test('Testando o componente renderiza corretamente',  () => {
+  test('Testando o componente renderiza corretamente',  () => {    
 const purchase = localStorage.getItem('inProcessPurchase');
 const { getByText, getByLabelText, getByTestId, debug } = renderWithRouter(<AdminOrders />);    
     debug();
     });
+  test('testando metodo', () => {
+    const wrapper = mount(<AdminOrders />);
+   console.log(wrapper.instance())
+  })
+
 });
